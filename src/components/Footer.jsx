037570ab58053e753ef648c831054e31c319a0ee
Kkidgamer <khaembagift@gmail.com>
2025-10-05 11:15:41 +0300

    new1

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => (
+    <footer style={{ background: 'var(--text-dark)', color: 'var(--white)', padding: '1.5rem 0', textAlign: 'center' }}>
+      <div className="container">
+        <p>&copy; 2025 Friends International Centre, Ngong Road, Nairobi | 
+          <a href="https://facebook.com/ficquakers" style={{ color: 'var(--primary-blue)', margin: '0 0.5rem' }}>Facebook</a> | 
+          <a href="https://youtube.com/channel/UCTvrcnYa84nBKMPxlRP2xLg" style={{ color: 'var(--primary-blue)' }}>YouTube</a>
+        </p>
+      </div>
+    </footer>
+  );
+  
+  export default Footer;
